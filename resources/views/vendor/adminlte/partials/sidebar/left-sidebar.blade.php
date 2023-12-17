845ae37d863f126aec528763b3e6d79a77ec4950
aipnurhayadi08 <aipnurhayadi08@gmail.com>
2023-12-17 13:19:26 +0700

    Expose: views laravel adminlte

---
+++ resources/views/vendor/adminlte/partials/sidebar/left-sidebar.blade.php
@@ -0,0 +1,36 @@
+<aside class="main-sidebar {{ config('adminlte.classes_sidebar', 'sidebar-dark-primary elevation-4') }}">
+
+    {{-- Sidebar brand logo --}}
+    @if (config('adminlte.logo_img_xl'))
+        @include('adminlte::partials.common.brand-logo-xl')
+    @else
+        @include('adminlte::partials.common.brand-logo-xs')
+    @endif
+
+    {{-- Sidebar menu --}}
+    <div class="sidebar">
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex align-items-center">
+            <div class="image">
+                <img src="{{ asset('images/svgs/profile-default.svg') }}" class="img-circle elevation-2" alt="User Image">
+            </div>
+            <div class="info">
+                <span class="text-light d-block text-truncate">{{ auth()->user()->resident->nama_lengkap }}</span>
+                <div class="user-roles">
+                    @foreach (auth()->user()->getRoleNames()->toArray() as $role)
+                        <div class="badge badge-success">{{ ucwords($role) }}</div>
+                    @endforeach
+                </div>
+            </div>
+        </div>
+        <nav class="pt-2">
+            <ul class="nav nav-pills nav-sidebar flex-column {{ config('adminlte.classes_sidebar_nav', '') }}"
+                data-widget="treeview" role="menu"
+                @if (config('adminlte.sidebar_nav_animation_speed') != 300) data-animation-speed="{{ config('adminlte.sidebar_nav_animation_speed') }}" @endif
+                @if (!config('adminlte.sidebar_nav_accordion')) data-accordion="false" @endif>
+                {{-- Configured sidebar links --}}
+                @each('adminlte::partials.sidebar.menu-item', $adminlte->menu('sidebar'), 'item')
+            </ul>
+        </nav>
+    </div>
+
+</aside>
