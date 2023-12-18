242101611c9a6e940a2912d1667abaa1a9447d52
vnskevin <vinsensiuskevinn@gmail.com>
2023-12-18 13:03:12 +0700

    coba bootstrap

--- resources/views/residents/view.blade.php
+++ resources/views/residents/view.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
 <div class="card" style="width: 18rem;">
-    <img src="https://www.google.com/imgres?imgurl=https%3A%2F%2Fmedia.licdn.com%2Fdms%2Fimage%2FD4E03AQHtMg5Xa8bsdg%2Fprofile-displayphoto-shrink_200_200%2F0%2F1643818193519%3Fe%3D2147483647%26v%3Dbeta%26t%3DBdvBXgTAylnURtRPnXcq0Hf5mC0VloJ4pIvYtH9tCQM&tbnid=kEqFqN4f6h0g4M&vet=12ahUKEwjG4fKbp5iDAxVdvWMGHSEBDh0QMygCegQIARBP..i&imgrefurl=https%3A%2F%2Fwww.linkedin.com%2Fpub%2Fdir%2FZachariah%2FSwift&docid=hud_D7KdYJsq7M&w=200&h=200&q=zachariah%20swift&ved=2ahUKEwjG4fKbp5iDAxVdvWMGHSEBDh0QMygCegQIARBP" class="card-img-top">
+    <img src="https://media.licdn.com/dms/image/D4E03AQHtMg5Xa8bsdg/profile-displayphoto-shrink_200_200/0/1643818193519?e=2147483647&v=beta&t=BdvBXgTAylnURtRPnXcq0Hf5mC0VloJ4pIvYtH9tCQM" class="card-img-top">
     <div class="card-body">
         <h5 class="card-title">Nama Lengkap</h5>
         <p class="card-text">{{$resident->nama_lengkap}}</p>
