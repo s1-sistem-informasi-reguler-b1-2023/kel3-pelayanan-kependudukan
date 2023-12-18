ca9656f7992ab98d485cf4aa9e94ab1ada0f2db6
vnskevin <vinsensiuskevinn@gmail.com>
2023-12-18 20:39:06 +0700

    hasOne

---
+++ database/migrations/2023_12_18_202856_add_foreign_key_to_residents.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('residents', function (Blueprint $table) {
+            $table->unsignedBigInteger('desa_id')->after('id');
+            $table->foreign('desa_id')
+                ->references('id')
+                ->on('residents');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropConstrainedForeignId('resident_id');
+        });
+    }
+};
