f76fd8af33b517093ae7c26439b36ba9e6452f9c
vnskevin <vinsensiuskevinn@gmail.com>
2023-12-18 14:43:41 +0700

    coba belongsto

--- app/Models/Territory.php
+++ app/Models/Territory.php
@@ -9,4 +9,9 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Territory extends Model
 {
     use HasFactory, SoftDeletes;
+
+    public function Resident()
+    {
+        return $this->belongsTo(Resident::class, 'desa_id', 'id');
+    }
 }
