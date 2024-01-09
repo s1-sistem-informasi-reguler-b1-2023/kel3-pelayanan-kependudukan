4648708ab55856c229d45d24a4c39569e0896f0f
aipnurhayadi08 <aipnurhayadi08@gmail.com>
2024-01-09 20:51:52 +0700

    template crud news

--- resources/views/news/create.blade.php
+++ resources/views/news/create.blade.php
@@ -3,9 +3,14 @@
 @section('title', 'News')
 
 @section('content_header')
-<h1 class="m-0">News</h1>
+    <h1 class="m-0">Tambah News</h1>
 @stop
 
 @section('content')
-Form Create
+    <div class="card">
+        <div class="card-header">Tambah News</div>
+        <div class="card-body">
+
+        </div>
+    </div>
 @stop
--- resources/views/news/view.blade.php
+++ resources/views/news/view.blade.php
@@ -3,9 +3,14 @@
 @section('title', 'News')
 
 @section('content_header')
-<h1 class="m-0">News</h1>
+    <h1 class="m-0">View News</h1>
 @stop
 
 @section('content')
-Views
+    <div class="card">
+        <div class="card-header">View News</div>
+        <div class="card-body">
+
+        </div>
+    </div>
 @stop
