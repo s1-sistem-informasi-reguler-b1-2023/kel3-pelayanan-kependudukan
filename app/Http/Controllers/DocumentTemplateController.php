505c19bb383a88ec73ec288a905af2d498b83f96
aipnurhayadi08 <aipnurhayadi08@gmail.com>
2024-01-16 13:33:42 +0700

    gg cuy

--- app/Http/Controllers/DocumentTemplateController.php
+++ app/Http/Controllers/DocumentTemplateController.php
@@ -2,7 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Helpers\DateHelper;
 use App\Models\DocumentTemplate;
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Str;
@@ -80,11 +82,47 @@ class DocumentTemplateController extends Controller
         return response()->json($formatted_items);
     }
 
+    function replacePlaceholders($template, $data)
+    {
+        preg_match_all('/\[([^\]]+)\]/', $template, $matches);
+        foreach ($matches[1] as $placeholder) {
+            $placeholderSplit = explode(':', $placeholder);
+            if (count($placeholderSplit) > 1) {
+                list($kategori, $nama_variabel) = $placeholderSplit;
+                if (isset($data[$kategori]) && isset($data[$kategori][$nama_variabel])) {
+                    $placeholderToReplace = "[$kategori:$nama_variabel]";
+
+                    $template = str_replace($placeholderToReplace, $data[$kategori]->{$nama_variabel}, $template);
+                }
+            } elseif (count($placeholderSplit) == 1) {
+                list($kategori) = $placeholderSplit;
+                if (isset($data[$kategori])) {
+                    $placeholderToReplace = "[$kategori]";
+                    $template = str_replace($placeholderToReplace, $data[$kategori], $template);
+                }
+            }
+        }
+
+        return $template;
+    }
+
     public function preview(DocumentTemplate $documentTemplate)
     {
+        $pengaju = auth()->user()->resident;
+        $rt = User::with('roles')->get()->filter(
+            fn($user) => $user->roles->where('name', 'rt')->toArray()
+        )->first()->resident;
+        $now = now();
+
+        $markdown = $this->replacePlaceholders($documentTemplate->konten, [
+            'pengaju' => $pengaju,
+            'rt' => $rt,
+            'now' => DateHelper::formatAsIndonesianDate(date('Y-m-d')),
+        ]);
+
         // Create the mPDF document
         $document = new Mpdf();
-        $document->WriteHTML(Str::of($documentTemplate->konten)->markdown());
+        $document->WriteHTML(Str::of($markdown)->markdown());
         $document->Output();
     }
 }
